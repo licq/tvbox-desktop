6ed3176f2b8d83ca91fa3240cc62c0638d2a2827
Chaoqun Li <lee.chaoqun@gmail.com>
2026-04-22 10:09:52 +0800

    feat: add wencai parsing service

--- src-tauri/src/services/mod.rs
+++ src-tauri/src/services/mod.rs
@@ -5,6 +5,7 @@ pub mod parser;
 pub mod resolver;
 pub mod storage;
 pub mod tvbox;
+pub mod wencai;
 pub mod xb6v;
 pub mod zxzj;
 
@@ -21,6 +22,7 @@ pub use parser::Parser;
 pub use resolver::{is_visible_playback_target, playback_sort_rank, PlaybackResolver};
 pub use storage::Storage;
 pub use tvbox::TvboxConfigParser;
+pub use wencai::{extract_player_url as extract_wencai_player_url, is_wencai_site};
 pub use xb6v::{
     scrape_catalog_detail_from_json, scrape_supported_tvbox_catalogs, ScrapedCatalogEpisode,
     ScrapedCatalogItem,
